9ef6bf6b7c83c2f2333bab4b9bdc6d6c9d0080c4
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-11-09 09:03:20 -0800

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -16,31 +16,27 @@ export async function getStaticProps() {
 export default function Home({ allDataMain }) {
   return (
     <Layout>
-       <strong>Breaking News - Sept 16, 2024</strong>
-      <div className="box p-2 m-4">
-       
-      <p > <strong className="large">Shōgun</strong> - &#8220;An adaptation from a 1975 best-selling book centered on 17th century feudal Japan on the brink of civil war, set a record on September 15, 2024 for <strong>most Emmys won by a show in a single year, winning 18 in all.</strong> It was also the first time a foreign language show (roughly 70 percent of the show was in Japanese) had taken the best drama award that is normally the domain of shows that take place in the United States, the United Kingdom or Westeros.&#8220; <strong>- The New York Times</strong></p>
-      </div>
+    
       <hr/>
-      <h1 className="text-center">Shōgun Characters and Cast</h1>
+      <h1 className="text-center">List of Posts</h1>
       <hr/>
-      <h2>Main Characters</h2>
+      <h2>Posts</h2>
       <div className="list-group">
-        {allDataMain.map(({ id, Character, gender }) => {
-          const genderClass = gender === 1 ? 'male' : 'female';
+        {allDataMain.map(({ id, Character }) => {
+          
           return (
-            <Link key={id} href={`/main/${id}`} className={`list-group-item list-group-item-action ${genderClass}`}>
+            <Link key={id} href={`/main/${id}`} className={`list-group-item list-group-item-action`}>
               {Character}
             </Link>
           );
         })}
       </div>
 
-      {/* Secondary Characters */}
+      {/* Secondary Characters
       <h4 className="mt-3">More Characters</h4>
       <Link href="/secondary" className="btn btn-secondary ">
         View Supporting Characters
-      </Link>
+      </Link> */}
     </Layout>
   );
 }
